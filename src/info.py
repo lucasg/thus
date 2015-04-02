8d96bd2b89f14efd4406194ca845b70dc1dee5d4
Philip <philm@manjaro.org>
2015-04-02 21:57:53 +0200

    0.8.13rc1 - update translations - depreciate kbctl

--- src/info.py
+++ src/info.py
@@ -26,6 +26,6 @@
 
 """ Set some Thus global constants """
 
-THUS_VERSION = "0.8.12"
-THUS_LAST_STABLE = "0.8.11"
+THUS_VERSION = "0.8.13rc1"
+THUS_LAST_STABLE = "0.8.12"
 THUS_WEBSITE = "http://www.manjaro.org"
