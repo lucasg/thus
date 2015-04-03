e58b745673f52eee6512e01b5c5251cd1b3ed6c4
Philip <philm@manjaro.org>
2015-04-03 16:32:40 +0200

    [thus] fix geometry issue

--- thus/main_window.py
+++ thus/main_window.py
@@ -187,13 +187,13 @@ class MainWindow(Gtk.ApplicationWindow):
 
         self.ui.connect_signals(self)
 
-        self.set_title(_("Manjaro Installer - Thus {0}".format(info.THUS_VERSION))
+        self.set_title(_("Manjaro Installer - Thus {0}".format(info.THUS_VERSION)))
 
-        # self.set_geometry()
+        self.set_geometry()
 
-        self.set_position(Gtk.WindowPosition.CENTER)
-        self.set_resizable(False)
-        self.set_size_request(MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT)
+        #self.set_position(Gtk.WindowPosition.CENTER)
+        #self.set_resizable(False)
+        #self.set_size_request(MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT)
 
         # Set window icon
         icon_path = os.path.join(data_dir, "images", "manjaro", "manjaro-icon.png")
