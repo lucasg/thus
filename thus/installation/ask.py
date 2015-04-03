7aa773a7e7b2e9a2dc07e560ab6631f113bb3a73
Philip <philm@manjaro.org>
2015-04-03 11:25:17 +0200

    Convert files - update ask - we need to check icon folders

--- thus/installation/ask.py
+++ thus/installation/ask.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  installation_ask.py
+#  ask.py
 #
 #  This file was forked from Cnchi (graphical installer from Antergos)
 #  Check it at https://github.com/antergos
 #
-#  Copyright 2013 Antergos (http://antergos.com/)
-#  Copyright 2013 Manjaro (http://manjaro.org)
+#  Copyright © 2013-2015 Antergos (http://antergos.com/)
+#  Copyright © 2013 Manjaro (http://manjaro.org)
 #
-#  This program is free software; you can redistribute it and/or modify
+#  This file is part of Thus.
+#
+#  Thus is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
 #
-#  This program is distributed in the hope that it will be useful,
+#  Thus is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
+#  along with Thus; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
 
@@ -180,10 +182,14 @@ class InstallationAsk(Gtk.Box):
         if self.next_page == "installation_automatic":
             self.settings.set('use_lvm', use_lvm)
             self.settings.set('use_luks', use_luks)
+            self.settings.set('use_luks_in_root', True)
+            self.settings.set('luks_root_volume', "cryptManjaro")
             self.settings.set('use_home', use_home)
         else:
             self.settings.set('use_lvm', False)
             self.settings.set('use_luks', False)
+            self.settings.set('use_luks_in_root', False)
+            self.settings.set('luks_root_volume', "")
             self.settings.set('use_home', False)
 
         if self.settings.get('use_luks'):
