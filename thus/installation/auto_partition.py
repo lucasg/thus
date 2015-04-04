1f6385bb2b166b795292a3bbef19d7dd24504275
Philip <philm@manjaro.org>
2015-04-04 20:20:32 +0200

    [thus] make automatic lvm non-user-interactive

--- thus/installation/auto_partition.py
+++ thus/installation/auto_partition.py
@@ -815,19 +815,23 @@ class AutoPartition(object):
 
             try:
                 size = str(int(part_sizes['root']))
-                cmd = ["lvcreate", "--name", "ManjaroRoot", "--size", size, "ManjaroVG"]
+                cmd = ["lvcreate", "--zero", "y", "--wipesignatures", "y", "--name", \
+                "ManjaroRoot", "--size", size, "ManjaroVG"]
                 subprocess.check_call(cmd)
 
                 if not self.home:
                     # Use the remainig space for our swap volume
-                    cmd = ["lvcreate", "--name", "ManjaroSwap", "--extents", "100%FREE", "ManjaroVG"]
+                    cmd = ["lvcreate", "--zero", "y", "--wipesignatures", "y", "--name", \
+                    "ManjaroSwap", "--extents", "100%FREE", "ManjaroVG"]
                     subprocess.check_call(cmd)
                 else:
                     size = str(int(part_sizes['swap']))
-                    cmd = ["lvcreate", "--name", "ManjaroSwap", "--size", size, "ManjaroVG"]
+                    cmd = ["lvcreate", "--zero", "y", "--wipesignatures", "y", "--name", \
+                    "ManjaroSwap", "--size", size, "ManjaroVG"]
                     subprocess.check_call(cmd)
                     # Use the remaining space for our home volume
-                    cmd = ["lvcreate", "--name", "ManjaroHome", "--extents", "100%FREE", "ManjaroVG"]
+                    cmd = ["lvcreate", "--zero", "y", "--wipesignatures", "y", "--name", \
+                    "ManjaroHome", "--extents", "100%FREE", "ManjaroVG"]
                     subprocess.check_call(cmd)
             except subprocess.CalledProcessError as err:
                 txt = _("Error creating LVM logical volume")
